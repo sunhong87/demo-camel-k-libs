403947c5efca3652b7c32d040be37069a2c642e7
thuan.ngo <thuan.ngo@kegmil.com>
2024-01-15 17:07:51 +0700

    init test project

---
+++ src/main/java/demo/camelk/models/HelloModel.java
@@ -0,0 +1,17 @@
+package demo.camelk.models;
+
+public class HelloModel {
+  private String name;
+
+  public HelloModel(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
