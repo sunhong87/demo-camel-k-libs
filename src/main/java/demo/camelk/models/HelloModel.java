2cf9b44d1d94040f833c5586a8afab8dd8449c39
thuan.ngo <thuan.ngo@kegmil.com>
2024-01-16 14:14:49 +0700

    update code

--- src/main/java/demo/camelk/models/HelloModel.java
+++ src/main/java/demo/camelk/models/HelloModel.java
@@ -1,5 +1,9 @@
 package demo.camelk.models;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.util.Optional;
+
 public class HelloModel {
   private String name;
 
@@ -13,5 +17,11 @@ public class HelloModel {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public static Optional<HelloModel> convertFromJson(String json) {
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    return Optional.ofNullable(json)
+        .map(jsonString -> gson.fromJson(jsonString, HelloModel.class));
   }
 }
